99219c7131f88620d4bd65f84c48f8b19f966af5
Sirui Zhang <105834027+SI-RUI-ZHANG@users.noreply.github.com>
2025-10-26 06:04:50 +0800

    Enable transparent background for rose-pine

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -1,8 +1,20 @@
 -- lua/plugins/rose-pine.lua
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine")
-	end
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+                require("rose-pine").setup({
+                        disable_background = true,
+                        disable_float_background = true,
+                        highlight_groups = {
+                                Normal = { bg = "none" },
+                                NormalNC = { bg = "none" },
+                                NormalFloat = { bg = "none" },
+                                FloatBorder = { bg = "none" },
+                                SignColumn = { bg = "none" },
+                        },
+                })
+
+                vim.cmd("colorscheme rose-pine")
+        end,
 }
